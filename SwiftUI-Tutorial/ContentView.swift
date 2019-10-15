08e63230dc267d6994ddbcedd2b004a997236866
Achsuthan <achsuthan@icloud.comS>
2019-10-15 10:32:17 +0530

    Initial Commit

---
+++ SwiftUI-Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Achsuthan Mahendran on 10/15/19.
+//  Copyright © 2019 Achsuthan Mahendran. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
